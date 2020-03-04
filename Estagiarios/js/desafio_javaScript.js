62441eced119f83ce49869a58be7de8d216fdeda
Leandro Cunha <leandro.jrc@hotmail.com>
2020-03-04 17:48:20 -0300

    adição de mascaras e validação.

--- Estagiarios/js/desafio_javaScript.js
+++ Estagiarios/js/desafio_javaScript.js
@@ -1,43 +1,16 @@
 function Enviar() {
- 
     var nome = document.getElementById("nomeid");
- 
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
  
 }
-function validaForm(frm) {
-    /*
-    o parâmetro frm desta função significa: this.form,
-    pois a chamada da função - validaForm(this) foi
-    definida na tag form.
-    */
-    //Verifica se o campo nome foi preenchido e
-    //contém no mínimo três caracteres.
-    if(frm.nome.value == " || form.nome.value == null || frm.nome.value.lenght < 3"){
-        //É mostrado um alerta, caso o campo esteja vazio.
-        alert("Por favor, indique o seu nome.");
-        //Foi definido um focus no campo.
-        frm.nome.focus();
-        //o form não é enviado.
-        return false;
-    }
-    //o campo e-mail precisa de conter: "@", "." e não pode estar vazio
-    if(frm.email.value.indexOf("@") == -1 ||
-      frm.email.valueOf.indexOf(".") == -1 ||
-      frm.email.value == "" ||
-      frm.email.value == null) {
-        alert("Por favor, indique um e-mail válido.");
-        frm.email.focus();
-        return false;
-    }
-}
+
 // JavaScript Document
 
 //adiciona mascara de cep
 function MascaraCep(cep){
-            if(mascaraInteiro(cep)==false){
+            if(MascaraCep(frm.cep)==false){
             event.returnValue = false;
     }       
     return formataCampo(cep, '00.000-000', event);
@@ -45,7 +18,7 @@ function MascaraCep(cep){
 
 //adiciona mascara de data
 function MascaraData(data){
-    if(mascaraInteiro(data)==false){
+    if(mascaraInteiro(frm.data)==false){
             event.returnValue = false;
     }       
     return formataCampo(data, '00/00/0000', event);
@@ -104,11 +77,43 @@ function cpfcnpj(v){
 
     return v
 }
+//Valida Form
+function validaForm(frm) {
+    /*
+    o parâmetro frm desta função significa: this.form,
+    pois a chamada da função - validaForm(this) foi
+    definida na tag form.
+    */
+    //Verifica se o campo nome foi preenchido e
+    //contém no mínimo três caracteres.
+    if(frm.nome.value == " || form.nome.value == null || frm.nome.value.lenght < 3"){
+        //É mostrado um alerta, caso o campo esteja vazio.
+        alert("Por favor, indique o seu nome.");
+        //Foi definido um focus no campo.
+        frm.nome.focus();
+        //o form não é enviado.
+        return false;
+    }
+    //o campo e-mail precisa de conter: "@", "." e não pode estar vazio
+    if(frm.email.value.indexOf("@") == -1 ||
+      frm.email.valueOf.indexOf(".") == -1 ||
+      frm.email.value == "" ||
+      frm.email.value == null) {
+        alert("Por favor, indique um e-mail válido.");
+        frm.email.focus();
+        return false;
+    }
+}
 
 //valida telefone
-function ValidaTelefone(tel){
+function ValidaTelefone(fonefixo){
     exp = /\(\d{2}\)\ \d{4}\-\d{4}/
-    if(!exp.test(tel.value))
+    if(!exp.test(fonefixo.value))
+            alert('Numero de Telefone Invalido!');
+}
+function ValidaTelefoneCelular(fonecelular){
+    exp = /\(\d{2}\)\ \d{5}\-\d{4}/
+    if(!exp.test(fonecelular.value))
             alert('Numero de Telefone Invalido!');
 }
 
@@ -127,7 +132,7 @@ function ValidaData(data){
 }
 
 //valida numero inteiro com mascara
-function mascaraInteiro(){
+function validaInteiro(){
     if (event.keyCode < 48 || event.keyCode > 57){
             event.returnValue = false;
             return false;
@@ -166,7 +171,5 @@ function formataCampo(campo, Mascara, evento) {
     }else { 
             return true; 
     }
-}
-function editJquery{
-    var itens = $("#lista li").detach();
+
 }
