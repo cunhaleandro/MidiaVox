26865ef4306ba25497bd214d5b498daba746779e
Leandro Cunha <leandro.jrc@hotmail.com>
2020-03-05 14:40:26 -0300

    Ajustes nomenclatura do form e revisão de parâmetros de mascaras e validações.

--- Estagiarios/js/desafio_javaScript.js
+++ Estagiarios/js/desafio_javaScript.js
@@ -4,7 +4,7 @@ function Enviar() {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
  
-}
+};
 
 // JavaScript Document
 
@@ -14,7 +14,7 @@ function MascaraCep(cep){
             event.returnValue = false;
     }       
     return formataCampo(cep, '00.000-000', event);
-}
+};
 
 //adiciona mascara de data
 function MascaraData(data){
@@ -22,7 +22,7 @@ function MascaraData(data){
             event.returnValue = false;
     }       
     return formataCampo(data, '00/00/0000', event);
-}
+};
 
 //adiciona mascara ao telefone
 function MascaraTelefone(tel){  
@@ -30,17 +30,17 @@ function MascaraTelefone(tel){
             event.returnValue = false;
     }       
     return formataCampo(tel, '(00) 0000-0000', event);
-}
+};
 
 function mascaramutua(o,f){
     v_obj=o
     v_fun=f
     setTimeout('execmascara()',1)
-}
+};
 
 function executamascaramutua(){
     v_obj.value=v_fun(v_obj.value)
-}
+};
 
 function cpfcnpj(v){
 
@@ -76,18 +76,10 @@ function cpfcnpj(v){
     }
 
     return v
-}
+};
 //Valida Form
 function validaForm(frm) {
-    /*
-    o parâmetro frm desta função significa: this.form,
-    pois a chamada da função - validaForm(this) foi
-    definida na tag form.
-    */
-    //Verifica se o campo nome foi preenchido e
-    //contém no mínimo três caracteres.
     if(frm.nome.value == " || form.nome.value == null || frm.nome.value.lenght < 3"){
-        //É mostrado um alerta, caso o campo esteja vazio.
         alert("Por favor, indique o seu nome.");
         //Foi definido um focus no campo.
         frm.nome.focus();
@@ -103,33 +95,33 @@ function validaForm(frm) {
         frm.email.focus();
         return false;
     }
-}
+};
 
 //valida telefone
 function ValidaTelefone(fonefixo){
     exp = /\(\d{2}\)\ \d{4}\-\d{4}/
     if(!exp.test(fonefixo.value))
             alert('Numero de Telefone Invalido!');
-}
+};
 function ValidaTelefoneCelular(fonecelular){
     exp = /\(\d{2}\)\ \d{5}\-\d{4}/
     if(!exp.test(fonecelular.value))
             alert('Numero de Telefone Invalido!');
-}
+};
 
 //valida CEP
 function ValidaCep(cep){
     exp = /\d{2}\.\d{3}\-\d{3}/
     if(!exp.test(cep.value))
             alert('Numero de Cep Invalido!');               
-}
+};
 
 //valida data
 function ValidaData(data){
     exp = /\d{2}\/\d{2}\/\d{4}/
     if(!exp.test(data.value))
             alert('Data Invalida!');                        
-}
+};
 
 //valida numero inteiro com mascara
 function validaInteiro(){
@@ -138,7 +130,31 @@ function validaInteiro(){
             return false;
     }
     return true;
-}
+};
+
+// Valida data
+function validaData(dataNascimento)
+{
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dataNascimento))
+        return false;
+    var partes = dataNascimento.split("/");
+    var dia = parseInt(partes[1], 10);
+    var mes = parseInt(partes[0], 10);
+    var ano = parseInt(partes[2], 10);
+
+    // Verifica os intervalos de mês e ano
+    if(ano < 1900 || ano> 3000 || mes == 0 || mes > 12)
+        return false;
+
+    var tamanhoMes = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Ajusta para os anos bissextos
+    if(ano % 400 == 0 || (ano % 100 != 0 && ano % 4 == 0))
+        tamanhoMes[1] = 29;
+
+    // Verifica o intervalo do dia
+    return dia > 0 && dia <= tamanhoMes[mes - 1];
+};
 
 //formata de forma generica os campos
 function formataCampo(campo, Mascara, evento) { 
@@ -172,4 +188,4 @@ function formataCampo(campo, Mascara, evento) {
             return true; 
     }
 
-}
+};
